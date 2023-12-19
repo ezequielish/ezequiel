df74746176ac7b547346813a80c3bdec65524360
ezequielish <ezequielprophetic@gmail.com>
2023-12-18 21:51:06 -0400

    feat: Website Personal
    - Seccion Aboutme y Experiencia

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://ezequielish.github.io/',
+    base: 'https://github.com/ezequielish/ezequielish.github.io',
+});
