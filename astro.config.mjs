160e8def973ff2f5d2a1054254c010ae3dbf5e6c
ezequielish <ezequielprophetic@gmail.com>
2023-12-19 08:25:21 -0400

    fix: Ajuste de ruta para los estilos e imagenes de sitio estatico

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ezequielish.github.io/',
-    base: 'https://github.com/ezequielish/ezequielish.github.io',
     outDir: './docs',
 });
